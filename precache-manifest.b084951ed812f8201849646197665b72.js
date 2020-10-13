3a97f57c90182e37c754b51391f88a42f7c16b3f
diksha-giri013 <diksha.giri013@gmail.com>
2020-10-14 01:18:21 +0530

    Updates

---
+++ precache-manifest.b084951ed812f8201849646197665b72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "892f6cf351b9101101c6303da56385b5",
+    "url": "/burger-temp00/index.html"
+  },
+  {
+    "revision": "148636dcd848b5fd7dfb",
+    "url": "/burger-temp00/static/css/main.6d5a0d07.chunk.css"
+  },
+  {
+    "revision": "9c88e25b0816e29eeff2",
+    "url": "/burger-temp00/static/js/2.d28a6a36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/burger-temp00/static/js/2.d28a6a36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "148636dcd848b5fd7dfb",
+    "url": "/burger-temp00/static/js/main.2493968a.chunk.js"
+  },
+  {
+    "revision": "28cddfeb7a3f9728209d",
+    "url": "/burger-temp00/static/js/runtime-main.074cfb18.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-temp00/static/media/burger-logo.b8503d26.png"
+  }
+]);
